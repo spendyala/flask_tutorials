b869adc4351932af5d9fbc974e4f97264bea21ab
Veera Pendyala <vpendyala@C02SH46LG8WP.corp.proofpoint.com>
2018-06-18 01:00:30 -0700

    POST request and README updates.

--- README.md
+++ README.md
@@ -22,3 +22,31 @@ virtualenv -p `which python3` .venv
 
 ## Run the server
 `python hello.py`
+
+
+## Some CLI commands we used
+`curl -XGET http://127.0.0.1:9500/f_to_c/%20105%20`
+`curl -XPOST http://127.0.0.1:9500/f_to_c/`
+
+```
+telnet 127.0.0.1 9500
+Trying 127.0.0.1...
+Connected to localhost.
+Escape character is '^]'.
+POST /f_to_c/ HTTP/1.0
+Host: 127.0.0.1:9500
+Content-Length: 3
+
+105
+HTTP/1.0 200 OK
+Content-Type: text/html; charset=utf-8
+Content-Length: 20
+Server: Werkzeug/0.14.1 Python/3.6.5
+Date: Mon, 18 Jun 2018 07:42:42 GMT
+
+This is POST RequestConnection closed by foreign host.
+```
+
+
+## Warning
+Please don't use `pdb` or `ipython` or `ipdb` in `requirements.txt` file
--- tutorial/hello.py
+++ tutorial/hello.py
@@ -1,19 +1,60 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
+# This is GET example
 @app.route('/')
 def hello():
     return 'Hello World!'
 
-
+# This is GET example
 @app.route('/name/<username>')
 def hello_user(username):
     # return 'Hello %s' % (username, ) ### Really really old formats
     # return 'Hello {}'.format(username)
     # return 'Hello {username}'.format(**{'username': username})
     return f'Hello {username}'
+
+# This is GET example
+@app.route('/f_to_c/help')
+def fahrenheit_to_celsius_help():
+    return 'Converts Temperatures from Fahrenheit to Celsius'
+
+
+def f_to_c_conversion(temp_f):
+    try:
+        temp_c = ((int(temp_f) - 32) * 5)/9
+        return str(temp_c), True
+    except TypeError as type_err:
+        return 'There is Type error, input value is not converted to Integer', False
+    except ValueError as val_err:
+        return f'Given value to convert from Fahrenheit to Celsius is not a valid integer {temp_f}', False
+   
+
+
+# This is GET example
+@app.route('/f_to_c/<temp_f>')
+def fahrenheit_to_celsius_convert(temp_f):
+    value, flag = f_to_c_conversion(temp_f)
+    if flag:
+       return value
+    else:
+       return value, 500
+
+
+# This is POST example
+@app.route('/f_to_c/', methods=['POST'])
+def fah_to_cel_conv_post():
+    import ipdb; ipdb.set_trace()
+    if request.method == 'POST':
+        value, flag = f_to_c_conversion(request.get_data())
+        if flag:
+           return value
+        else:
+           return value, 500
+    else:
+        return 'This is an invalid request', 500
 
 
 if __name__ == '__main__':
