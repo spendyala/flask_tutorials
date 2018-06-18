108e524470029170f28368058f16dd76cecbb098
Veera Pendyala <vpendyala@C02SH46LG8WP.corp.proofpoint.com>
2018-06-17 22:12:14 -0700

    Request with one parameter

--- tutorial/hello.py
+++ tutorial/hello.py
@@ -8,6 +8,14 @@ def hello():
     return 'Hello World!'
 
 
+@app.route('/name/<username>')
+def hello_user(username):
+    # return 'Hello %s' % (username, ) ### Really really old formats
+    # return 'Hello {}'.format(username)
+    # return 'Hello {username}'.format(**{'username': username})
+    return f'Hello {username}'
+
+
 if __name__ == '__main__':
     app.run(
         host='127.0.0.1',
